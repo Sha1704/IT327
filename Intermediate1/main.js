febcc8d83bf447377e33afe3bfc49165d32f82f2
Johnwirth28 <johnwirth28@gmail.com>
2025-05-05 07:07:54 -0500

    Intermediate 1

---
+++ Intermediate1/main.js
@@ -0,0 +1,31 @@
+/**
+ * This is the To-Do list main module which takes the functions in pgm01.js and runs them inputting them into a website to make the list.
+ * 
+ * This module contains the core logic for a 
+ * 
+ * Author: John Wirth
+ * Date: May 4, 2025
+ */
+import { List } from './pgm01.js';
+
+document.getElementById("start-btn").addEventListener("click", () => {
+    const list = new List();
+
+    function askNext() {
+        const answer = prompt("Would you like to add a task? (y/n)");
+        if (answer && answer.toLowerCase() === 'y') {
+            list.userInput();
+            askNext();
+        } else {
+            const tasks = list.getList();
+    
+            if (tasks.length === 0) {
+                document.getElementById("output").textContent = "No tasks were added.";
+            } else {
+                list.printList(); 
+            }
+        }
+    }
+
+    askNext();
+});
---
+++ Intermediate1/pgm01.js
@@ -0,0 +1,63 @@
+/**
+ * To-Do list Logic
+ * 
+ * This module contains the core logic for a To-Do List.
+ * 
+ * Author: A John Wirth
+ * Date: May 4, 2025
+ */
+export class List 
+{
+    constructor() 
+    {
+        this.list = [];
+    }
+
+    addToList(name, priority) 
+    {
+        const insertTask = 
+        { // holds the values of the task
+            name: name,
+            priority: parseInt(priority)
+        };
+
+        this.list.push(insertTask); //adds to the list
+
+        const listSize = this.list.length - 1;
+        for (let i = 0; i < listSize; i++) 
+        { //sorts the list
+            if (this.list[i].priority > this.list[listSize].priority) 
+            {
+                const temp = this.list[listSize];
+                this.list[listSize] = this.list[i];
+                this.list[i] = temp;
+            }
+        }
+    }
+
+    getList() //returns list
+    { 
+        return this.list;
+    }
+
+    userInput() //takes user input for another task
+    {
+        const name = prompt("Enter the name of the task you are doing:");
+        const priority = prompt("Enter the priority level for this task:");
+        this.addToList(name, priority);
+    }
+
+    printList() //prints the list
+    {
+        const output = document.getElementById("output");
+        output.innerHTML = '';
+
+        this.list.forEach((item, i) => 
+        {
+            const taskDiv = document.createElement('div');
+            taskDiv.classList.add('task-item');
+            taskDiv.textContent = `${i + 1}. ${item.name} Priority: ${item.priority}`;
+            output.appendChild(taskDiv);
+        });
+    }
+}
