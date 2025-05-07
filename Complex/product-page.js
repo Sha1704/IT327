5d65683197d32e27e9a2d422f35cc4ffb4cfbb40
Bvelich <bvelich@ilstu.edu>
2025-05-07 14:05:31 -0500

    Added the ability to advance the queue

--- Complex/product-page.js
+++ Complex/product-page.js
@@ -5,8 +5,10 @@ import Product from "./ProductClass.js";
 const productInfo = document.getElementById("productinfo");
 const customerFields = document.getElementById("customerFields");
 const queueStatus = document.getElementById("queueStatus");
+const debug = document.getElementById("debug");
 
 const product = new Product();
+const queue = new CustomerQueue();
 console.log(product.displayInfo());
 
 const productNameElement = document.createElement('p');
@@ -31,6 +33,12 @@ submitButton.id = "submitButton";
 submitButton.textContent ="Submit";
 
 const queueLocation = document.createElement('p');
+const estimatedTime = document.createElement('p');
+
+const debugInfo = document.createElement('p');
+const advanceButton = document.createElement('button');
+advanceButton.id = "advanceButton";
+advanceButton.textContent = "Advance Queue";
 
 document.getElementById("buyButton").addEventListener("click", function() {
 	console.log("Buy button clicked");
@@ -45,23 +53,33 @@ submitButton.addEventListener("click", function () {
 	console.log(customerName);
 	let customer = new Customer(customerName, 1);
 	customer.printInfo();
-
-	let queue = generateQueue();
+	generateQueue(queue);
 	queue.enqueue(customer);
 	console.log(queue.size());
 	let place = queue.size() - 1;
 	queueLocation.textContent = ("There are " + place + " people in front of you, " + queue.getHead().customer.getName() + " is first!");
-	queueStatus.appendChild(queueLocation)
-	console.log(queue.dequeue().printInfo());
-	console.log(queue.size());
+	let eta = place * 5;
+	estimatedTime.textContent = ("Your estimated wait is " + eta + " mins!");
+	queueStatus.appendChild(queueLocation);
+	queueStatus.appendChild(estimatedTime);
+
+	debugInfo.textContent = "The following button simulates the passage of 5 mins";
+	debug.appendChild(debugInfo)
+	debug.appendChild(advanceButton);
 })
 
-function generateQueue(){
-	let queue = new CustomerQueue();
-	queue.enqueue(new Customer("Billy",2));
-	queue.enqueue(new Customer("Tom",3));
-	queue.enqueue(new Customer("George",4));
-	queue.enqueue(new Customer("Sarah",5));
-	queue.enqueue(new Customer("Lee",6));
-	return queue;
+advanceButton.addEventListener("click", function() {
+	queue.dequeue();
+	let place = queue.size() - 1;
+	let eta = place * 5;
+	queueLocation.textContent = ("There are " + place + " people in front of you, " + queue.getHead().customer.getName() + " is first!");
+	estimatedTime.textContent = ("Your estimated wait is " + eta + " mins!");
+})
+
+function generateQueue(inputQueue){
+	inputQueue.enqueue(new Customer("Billy",2));
+	inputQueue.enqueue(new Customer("Tom",3));
+	inputQueue.enqueue(new Customer("George",4));
+	inputQueue.enqueue(new Customer("Sarah",5));
+	inputQueue.enqueue(new Customer("Lee",6));
 }
